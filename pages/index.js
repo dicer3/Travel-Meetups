4c8b95b45dadfec0f885f3b4151f0f665583fc32
pragungandotra@gmail.com <pragungandotra7@gmail.com>
2021-09-16 22:38:54 +0530

    created different pages and navigated b/w them

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
-// https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg
-
 import MeetupList from "../components/meetups/MeetupList";
-
 const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address .. 1",
+    description: "this is a first meetup !!",
+  },
+  {
+    id: "m2",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address .. 2",
+    description: "this is a second meetup !!",
+  },
+  {
+    id: "m3",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address .. 3",
+    description: "this is a third meetup !!",
   },
 ];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
