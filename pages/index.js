aa17245ba1812b1f62f16bab1d8be124a7446ef7
pragungandotra@gmail.com <pragungandotra7@gmail.com>
2021-09-16 16:41:41 +0530

    intialized git project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+  },
+];
